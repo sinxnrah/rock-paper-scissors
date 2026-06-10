98fce4c1b76e2f61c18c82768598d75021ec8359
sinxnrah <sinxnrahman8d@gmail.com>
2026-06-10 00:54:23 -0400

    Implement single round logic

--- script.js
+++ script.js
@@ -4,4 +4,65 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() { 
+    return prompt("Rock, paper, or scissors?")
+}
+
+// player score variables
+let humanScore = 0, computerScore = 0;
+
+
+/**
+ * @param {string} humanChoice
+ * @param {string} computerChoice
+ */
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
+    if (humanChoice === computerChoice) {console.log("Draw!");}
+    else if (humanChoice === `Rock` && computerChoice === `Scissors` ||
+            humanChoice === `Paper` && computerChoice === 'Rock' ||
+            humanChoice === `Scissors` && computerChoice === `Paper`) 
+            {
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+                humanScore++;                        
+            }
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+
+/*
+    if human equals computer:
+        print tie statement
+    else If human = rock and computer = paper:
+        print lose statement
+        increment winner
+    else if human = rock and computer = scissors:
+        print win statement
+        increment winner
+    else if human = paper and computer = scissors:
+        print lose statement
+        increment winner
+    else if human = paper and computer = rock:
+        print win statement
+        increment winner
+    else if human = scissors and computer = rock:
+        print lose statement
+        increment winner
+    else if human = scissors and computer = paper:
+        print win statement
+        increment winner
+*/
+
+
+
+
+
